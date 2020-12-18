014d33a8d7aa4babf845080dcdea602e84d2da70
samurollie <53665491+samurollie@users.noreply.github.com>
2020-12-18 18:42:29 -0300

    Ultimo modulo, lets goooo

--- bin/main.dart
+++ bin/main.dart
@@ -1,6 +1,9 @@
 import 'package:projeto_dart/projeto_dart.dart'; // Importa a biblioteca projeto_dart
 
-void main(List<String> arguments) { // A função main pode receber argumentos, estes argumentos são passados através do terminal
+void main(List<String> arguments) {
+  // A função main pode receber argumentos, estes argumentos são passados através do terminal
   print(
       calculate()); // Usando a função calculate, que está dentro da biblioteca projeto_dart
+
+  // Da pra fazer o mesmo com os outros arquivos que estão na pasta src, mas eu to com preguiça :P
 }
